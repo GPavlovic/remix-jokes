51b192e8cdcfa6389095d26d4403773d8b739b64
Goran Pavlovic <gp.goranpavlovic@gmail.com>
2022-01-31 21:08:05 -0500

    Adding jokes display

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,52 +1,71 @@
-import type { LinksFunction } from "remix";
+import { LinksFunction, LoaderFunction, useLoaderData } from "remix";
 import { Outlet, Link } from "remix";
+import { db } from "~/utils/db.server";
 import stylesUrl from "../styles/jokes.css";
 
 export const links: LinksFunction = () => {
-  return [
-    {
-      rel: "stylesheet",
-      href: stylesUrl
-    }
-  ];
+    return [
+        {
+            rel: "stylesheet",
+            href: stylesUrl
+        }
+    ];
+};
+
+type LoaderData = {
+    jokeListItems: Array<{ id: string; name: string }>;
+};
+
+export const loader: LoaderFunction = async () => {
+    return {
+        jokeListItems: await db.joke.findMany({
+        take: 5,
+        select: { id: true, name: true },
+        orderBy: { createdAt: "desc" }
+      })
+    };
 };
 
 export default function JokesRoute() {
-  return (
-    <div className="jokes-layout">
-      <header className="jokes-header">
-        <div className="container">
-          <h1 className="home-link">
-            <Link
-              to="/"
-              title="Remix Jokes"
-              aria-label="Remix Jokes"
-            >
-              <span className="logo">🤪</span>
-              <span className="logo-medium">J🤪KES</span>
-            </Link>
-          </h1>
+    const jokes = useLoaderData<LoaderData>();
+
+    return (
+        <div className="jokes-layout">
+            <header className="jokes-header">
+                <div className="container">
+                    <h1 className="home-link">
+                        <Link
+                            to="/"
+                            title="Remix Jokes"
+                            aria-label="Remix Jokes"
+                        >
+                            <span className="logo">🤪</span>
+                            <span className="logo-medium">J🤪KES</span>
+                        </Link>
+                    </h1>
+                </div>
+            </header>
+            <main className="jokes-main">
+                <div className="container">
+                    <div className="jokes-list">
+                        <Link to=".">Get a random joke</Link>
+                        <p>Here are a few more jokes to check out:</p>
+                        <ul>
+                            {jokes.jokeListItems.map(joke => (
+                                <li>
+                                    <Link to={joke.id}>{joke.name}</Link>
+                                </li>
+                            ))}
+                        </ul>
+                        <Link to="new" className="button">
+                            Add your own
+                        </Link>
+                    </div>
+                    <div className="jokes-outlet">
+                        <Outlet />
+                    </div>
+                </div>
+            </main>
         </div>
-      </header>
-      <main className="jokes-main">
-        <div className="container">
-          <div className="jokes-list">
-            <Link to=".">Get a random joke</Link>
-            <p>Here are a few more jokes to check out:</p>
-            <ul>
-              <li>
-                <Link to="some-joke-id">Hippo</Link>
-              </li>
-            </ul>
-            <Link to="new" className="button">
-              Add your own
-            </Link>
-          </div>
-          <div className="jokes-outlet">
-            <Outlet />
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+    );
 }

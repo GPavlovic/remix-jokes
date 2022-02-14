250b09228647f8b4168213d5500f8e98f994fdba
Goran Pavlovic <gp.goranpavlovic@gmail.com>
2022-02-13 22:36:53 -0500

    Adding catch boundaries; Adding delete button

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,5 +1,5 @@
 import { Joke } from "@prisma/client";
-import { Link, LoaderFunction, useLoaderData } from "remix";
+import { Link, LoaderFunction, useCatch, useLoaderData } from "remix";
 import { db } from "~/utils/db.server";
 
 type LoaderData = {
@@ -13,6 +13,11 @@ export const loader: LoaderFunction = async () => {
         take: 1,
         skip: randomRowNumber
     });
+    if (!randomJoke) {
+        throw new Response("No random joke found", {
+            status: 404
+        });
+    }
     return { randomJoke };
 };
 
@@ -27,6 +32,21 @@ export default function JokesIndexRoute() {
                 "{data.randomJoke.name}" Permalink
             </Link>
         </div>
+    );
+}
+
+export function CatchBoundary() {
+    const caught = useCatch();
+
+    if (caught.status === 404) {
+        return (
+            <div className="error-container">
+                There are no jokes to display.
+            </div>
+        );
+    }
+    throw new Error(
+        `Unexpected caught response with status: ${caught.status}`
     );
 }
 
--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,6 +1,16 @@
-import { ActionFunction, json, redirect, useActionData } from "remix";
+import { ActionFunction, json, Link, LoaderFunction, redirect, useActionData, useCatch } from "remix";
 import { db } from "~/utils/db.server";
-import { requireUserId } from "~/utils/session.server";
+import { getUserId, requireUserId } from "~/utils/session.server";
+
+export const loader: LoaderFunction = async ({
+    request
+}) => {
+    const userId = await getUserId(request);
+    if (!userId) {
+        throw new Response("Unauthorized", { status: 401 });
+    }
+    return {};
+};
 
 function validateJokeContent(content: string) {
     if (content.length < 10) {
@@ -125,6 +135,19 @@ export default function NewJokeRoute() {
                 </div>
             </form>
         </div>);
+}
+
+export function CatchBoundary() {
+    const caught = useCatch();
+
+    if (caught.status === 401) {
+        return (
+            <div className="error-container">
+                <p>You must be logged in to create a joke.</p>
+                <Link to="/login">Login</Link>
+            </div>
+        );
+    }
 }
 
 export function ErrorBoundary() {
